6f589e47bf8d2f532f2fa45bc8fa807ccc861d9b
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-04-29 11:35:16 +0530

    Update Chat.js

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -26,6 +26,7 @@ export const Chat=(props)=>
 
         });
         return ()=>unsuscribe();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
     const handleSubmit =async(e)=>
